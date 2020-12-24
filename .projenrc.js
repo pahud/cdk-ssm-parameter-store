8d981bc26a1bf109e9e565779910bebc2d881111
Pahud <pahudnet@gmail.com>
2020-12-24 21:53:56 +0800

    update projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,11 @@
-const { AwsCdkConstructLibrary, GithubWorkflow } = require('projen');
+const { AwsCdkConstructLibrary } = require('projen');
 
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
   authorAddress: "pahudnet@gmail.com",
   authorName: "Pahud Hsieh",
-  cdkVersion: "1.70.0",
+  cdkVersion: "1.80.0",
   name: "cdk-ssm-parameter-store",
   repository: "https://github.com/pahud/cdk-ssm-parameter-store.git",
   cdkDependencies: [
@@ -33,11 +33,11 @@ const project = new AwsCdkConstructLibrary({
 });
 
 // create a custom projen and yarn upgrade workflow
-const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+workflow = project.github.addWorkflow('ProjenYarnUpgrade');
 
 workflow.on({
   schedule: [{
-    cron: '11 0 * * *'
+    cron: '11 0 * * *',
   }], // 0:11am every day
   workflow_dispatch: {}, // allow manual triggering
 });
@@ -47,14 +47,16 @@ workflow.addJobs({
     'runs-on': 'ubuntu-latest',
     'steps': [
       { uses: 'actions/checkout@v2' },
-      { 
+      {
         uses: 'actions/setup-node@v1',
         with: {
           'node-version': '10.17.0',
-        }
+        },
       },
-      { run: `yarn upgrade` },
-      { run: `yarn projen:upgrade` },
+      // { run: `yarn install` },
+      // { run: `yarn projen` },
+      { run: 'yarn upgrade' },
+      { run: 'yarn projen:upgrade' },
       // submit a PR
       {
         name: 'Create Pull Request',
@@ -66,11 +68,12 @@ workflow.addJobs({
           'title': 'chore: upgrade projen and yarn',
           'body': 'This PR upgrades projen and yarn upgrade to the latest version',
           'labels': 'auto-merge',
-        }
+        },
       },
     ],
   },
 });
+
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log', 
 'docker-image-publish.yml', 'utils'];
